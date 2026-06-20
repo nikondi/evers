be41df2f9383f17274d6306e297bd1140f6dbe92
nikondi <nikondi@narod.ru>
2026-06-20 22:36:00 +0300

    Welcome about

--- resources/views/pages/welcome.php
+++ resources/views/pages/welcome.php
@@ -8,3 +8,50 @@
         </div>
     </div>
 </section>
+
+<section class="welcome-about oversection">
+    <div class="container">
+        <div class="section-badge">О компании</div>
+        <h2 class="about-title">Эверс груп Рус&nbsp;— производитель <span>медицинских изделий</span>, ранозаживляющих
+            средств и&nbsp;уходовой косметики.</h2>
+        <div class="about-info">
+            <div class="about-metrics">
+                <div class="about-metric">
+                    <div class="about-metric__title">ГОСТ ISO 13485-2017</div>
+                    <div class="about-metric__text">вся продукция соответствует требованиям Системы менеджмента качества
+                    </div>
+                </div>
+                <div class="about-metric">
+                    <div class="about-metric__title">150+</div>
+                    <div class="about-metric__text">сотрудников компании ежедневно работают, чтобы&nbsp;сделать качество
+                        жизни в&nbsp;нашей стране выше
+                    </div>
+                </div>
+                <div class="about-metric">
+                    <div class="about-metric__title">3</div>
+                    <div class="about-metric__text">современные производственные площадки, оснащенные
+                        высокотехнологичным оборудованием в Московской и Ивановской областях
+                    </div>
+                </div>
+            </div>
+            <div class="about-info-text">
+                <p>Мы&nbsp;осуществляем полный цикл производства импортозамещающих медицинских изделий и&nbsp;уходовой
+                    косметики в&nbsp;России.</p>
+                <p>Обеспечиваем специалистов и&nbsp;широкий круг потребителей современными, эффективными, безопасными
+                    и&nbsp;доступными медицинскими изделиями.</p>
+                <div class="about-info-text__btn">
+                    <a href="#" class="btn">Узнать больше</a>
+                </div>
+                <div class="about-info-text-years">
+                    <div class="about-info-text-year"><b>С&nbsp;2017&nbsp;года&nbsp;—</b> представитель биомедицинского
+                        кластера ОЭЗ&nbsp;«Дубна»
+                    </div>
+                    <div class="about-info-text-year"><b>С&nbsp;2019&nbsp;года&nbsp;—</b> участник
+                        Медико-технологического
+                        кластера Московской области
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
